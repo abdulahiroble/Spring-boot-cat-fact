c3096ca903aa4773a2b61b7208fcd8e36fdf6f52
abdulahiroble <abdulahi_2610@hotmail.com>
2020-09-13 20:22:50 +0200

    first commit

---
+++ src/main/java/com/catfact/catfact/CatfactApplication.java
@@ -0,0 +1,13 @@
+package com.catfact.catfact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatfactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatfactApplication.class, args);
+	}
+
+}

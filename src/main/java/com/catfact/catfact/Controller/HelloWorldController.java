a4878d559250fa97ecb6f5e6a7040de224d8c7c1
abdulahiroble <abdulahi_2610@hotmail.com>
2020-09-15 12:45:45 +0200

    Move file to controller folder

--- src/main/java/com/catfact/catfact/Controller/HelloWorldController.java
+++ src/main/java/com/catfact/catfact/Controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.catfact.catfact;
+package com.catfact.catfact.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
